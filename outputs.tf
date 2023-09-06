d1de7050e98c9faa7cf94b31ad070f66bb3dd629
Ankit Charolia <charolia.a@zdf.de>
2023-09-06 22:24:23 +0200

    update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "GCP_WORKLOAD_IDENTITY_PROVIDER" {
-  value = google_iam_workload_identity_pool_provider.gitlab-provider-jwt.name
+  value = google_iam_workload_identity_pool_provider.this.name
 }
 
 output "GCP_SERVICE_ACCOUNT" {
-  value = google_service_account.gitlab-runner.email
+  value = google_service_account.this.email
 }
