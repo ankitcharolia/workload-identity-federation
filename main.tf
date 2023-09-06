64031cfaefb239019e03c7dff5563d297c80b272
Ankit Charolia <charolia.a@zdf.de>
2023-09-06 20:00:39 +0200

    refactor terraform code

--- main.tf
+++ main.tf
@@ -1,14 +1,16 @@
 # GCP Overview and Instructions
 # https://cloud.google.com/iam/docs/configuring-workload-identity-federation#oidc
 
-resource "google_iam_workload_identity_pool" "gitlab_pool" {
-  workload_identity_pool_id = "gitlab-ci"
+resource "google_iam_workload_identity_pool" "this" {
   project                   = var.project
+  workload_identity_pool_id = "gitlab-ci"
+  display_name              = "gitlab-ci"
+  description               = "Workload Identity pool for GitLab-CI"
 }
 
-resource "google_iam_workload_identity_pool_provider" "gitlab_provider_jwt" {
-  workload_identity_pool_id             = google_iam_workload_identity_pool.gitlab_pool.workload_identity_pool_id
-  workload_identity_pool_provider_id    = "artifact-registry"
+resource "google_iam_workload_identity_pool_provider" "this" {
+  workload_identity_pool_id             = google_iam_workload_identity_pool.this.workload_identity_pool_id
+  workload_identity_pool_provider_id    = var.provider_id
   project                               = var.project
   attribute_mapping = {
     "google.subject"           = "assertion.sub", # Required
@@ -21,21 +23,33 @@ resource "google_iam_workload_identity_pool_provider" "gitlab_provider_jwt" {
   }
   oidc {
     issuer_uri        = var.gitlab_url
-    allowed_audiences = [var.gitlab_url]
   }
 }
 
-resource "google_service_account" "gitlab_runner" {
-  account_id   = "gitlab-ci"
-  display_name = "[Gitlab-CI] Service Account for pushing the images to Google Artifact Registry"
+resource "google_service_account" "this" {
+  project      = var.project
+  account_id   = var.service_account
+  display_name = var.service_account_display_name
+  description  = "[Gitlab-CI] ${var.service_account_description}"
 }
 
-resource "google_service_account_iam_binding" "gitlab_runner_oidc" {
-  service_account_id = google_service_account.gitlab_runner.name
-  role               = "roles/iam.workloadIdentityUser"
-
-  members = [
-    "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.gitlab_pool.name}/attribute.project_id/${var.project}"
+resource "google_project_iam_member" "this" {
+  project = var.project
+  role    = var.role
+  member  = "serviceAccount:${google_service_account.this.email}"
+  depends_on = [
+    google_service_account.this
   ]
 }
 
+resource "google_service_account_iam_binding" "this" {
+  service_account_id = google_service_account.this.name
+  role               = "roles/iam.workloadIdentityUser"
+
+  members = [
+    "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.this.name}/attribute.project_path/${var.gitlab_project_path}"
+  ]
+  depends_on = [
+    google_service_account.this
+  ]
+}
--- variables.tf
+++ variables.tf
@@ -3,7 +3,7 @@ variable "gitlab_url" {
   default = "https://gitlab.com"
 }
 
-variable "gitlab_project_id" {
+variable "gitlab_project_path" {
   type        = string
   description = "Project ID to restrict authentication from."
 }
@@ -11,4 +11,16 @@ variable "gitlab_project_id" {
 variable "project" {
   type        = string
   description = "GCP Project name"
+}
+
+variable "service_account" {
+  type = string
+}
+
+variable "service_account_description" {
+  type = string
+}
+
+variable "service_account_display_name" {
+  type = string
 }
